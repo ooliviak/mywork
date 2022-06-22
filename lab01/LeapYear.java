ae5c94456568e59836e56da52daa0d6df72cc0ec
Olivia Kim <ogkim@berkeley.edu>
2022-06-22 10:30:57 -0700

    lab01: Completed LeapYear.java

--- lab01/LeapYear.java
+++ lab01/LeapYear.java
@@ -9,7 +9,7 @@ public class LeapYear {
      * @source CS 61BL Lab 1
      */
     public static boolean isLeapYear(int year) {
-        if (year % 40 == 0 || (year % 40 == 0 && year % 100 != 0)) {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
             return true;
         }
         else {
