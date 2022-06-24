5563e9499ffbcf4637739d18fe2228139e580189
Olivia Kim <ogkim@berkeley.edu>
2022-06-23 19:53:19 -0700

    final path

--- lab02/Path.java
+++ lab02/Path.java
@@ -1,6 +1,43 @@
 /** A class that represents a path via pursuit curves. */
 public class Path {
+    private Point curr;
+    private Point next;
+    private Point point;
 
-    // TODO
+    public Path(double x, double y) {
+        this.next = new Point(x, y);
+        this.curr = new Point(0,0);
+    }
+
+    public double getCurrX() {
+        return curr.getX();
+    }
+
+    public double getCurrY() {
+        return curr.getY();
+    }
+    public double getNextX() {
+        return next.getX();
+    }
+
+    public double getNextY() {
+        return next.getY();
+    }
+
+    public Point getCurrentPoint(){
+        return new Point(getCurrX(), getCurrY());
+    }
+
+    public void setCurrentPoint(Point point) {
+        curr = point;
+    }
+
+    public void iterate(double dx, double dy) {
+        curr = next;
+        next = new Point(curr.getX()+ dx, curr.getY() + dy );
+    }
+
+
+
 
 }
