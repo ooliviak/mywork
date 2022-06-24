0680fc7bb22a4382480592567f3d3d48d372ff11
Olivia Kim <ogkim@berkeley.edu>
2022-06-23 19:49:02 -0700

    account final

--- lab02/Account.java
+++ lab02/Account.java
@@ -5,12 +5,25 @@
 public class Account {
 
     private int balance;
+    private int leftover;
+//     private Account parent;
+    private Account parentAccount;
 
     /** Initialize an account with the given balance. */
     public Account(int balance) {
         this.balance = balance;
+        this.parentAccount = null;
     }
 
+//    public Account(int balance, Account parent) {
+//        this.balance = balance;
+//        this.parent = parent;
+//    }
+
+    public Account(int balance, Account parentAccount) {
+        this.balance = balance;
+        this.parentAccount = parentAccount;
+    }
     /** Returns the balance for the current account. */
     public int getBalance() {
         return balance;
@@ -29,17 +42,39 @@ public class Account {
      * Subtract amount from the account if possible. If subtracting amount
      * would leave a negative balance, print an error message and leave the
      * balance unchanged.
+     *
+     * @return
      */
-    public void withdraw(int amount) {
-        // TODO
-        if (amount < 0) {
-            System.out.println("Cannot withdraw negative amount.");
-        } else if (balance < amount) {
-            System.out.println("Insufficient funds");
+    public boolean withdraw(int amount) {
+        if (parentAccount == null) {
+            if (amount < 0) {
+                System.out.println("Cannot withdraw negative amount.");
+                return false;
+            } else if (balance < amount) {
+                System.out.println("Insufficient funds");
+                return false;
+            } else {
+                balance -= amount;
+                return true;
+            }
         } else {
-            balance -= amount;
+            if (amount <= balance ) {
+                balance -= amount;
+                return true;
+            } else if (amount > (balance + parentAccount.balance)) {
+                return false;
+            } else {
+                leftover = amount - balance;
+                withdraw(leftover);
+                balance = 0;
+                parentAccount.balance = parentAccount.balance - leftover;
+                return true;
+
+            }
+
         }
     }
+
 
     /**
      * Merge account other into this account by removing all money from other
@@ -47,5 +82,10 @@ public class Account {
      */
     public void merge(Account other) {
         // TODO
+        this.balance += other.balance;
+        other.balance = 0;
+
     }
+
+
 }
