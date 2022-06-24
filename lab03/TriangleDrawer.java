647c75ca510c87c42283e2853c97d12962025c6d
Olivia Kim <ogkim@berkeley.edu>
2022-06-24 13:13:00 -0700

    final tri commit

--- lab03/TriangleDrawer.java
+++ lab03/TriangleDrawer.java
@@ -1,26 +1,19 @@
 public class TriangleDrawer {
     public static void main(String[] args) {
-        col = col + 1;
-        int col = 0;
         int row = 0;
         int SIZE = 10;
-        row = row + 1;
-        System.out.print('*');
-        System.out.println('*');
-        System.out.println();
-        while (col <= row) {
 
-        }
-            while (col < row) {
-
+        while (row < SIZE) {
+            int col = 0;
+            while (col <= row) {
+                System.out.print('*');
+                col = col + 1;
             }
-                while (row < SIZE) {
-
-                }
-                    while (row <= SIZE) {
-
-                    }
+            System.out.println();
+            row = row + 1;
+        }
 
     }
+
 }
 
