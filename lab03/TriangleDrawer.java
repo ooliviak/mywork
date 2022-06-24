3ba5ee11a64d324fa63492faf02ccecc9f186ddd
Olivia Kim <ogkim@berkeley.edu>
2022-06-24 10:05:59 -0700

    first triangle commit

---
+++ lab03/TriangleDrawer.java
@@ -0,0 +1,26 @@
+public class TriangleDrawer {
+    public static void main(String[] args) {
+        col = col + 1;
+        int col = 0;
+        int row = 0;
+        int SIZE = 10;
+        row = row + 1;
+        System.out.print('*');
+        System.out.println('*');
+        System.out.println();
+        while (col <= row) {
+
+        }
+            while (col < row) {
+
+            }
+                while (row < SIZE) {
+
+                }
+                    while (row <= SIZE) {
+
+                    }
+
+    }
+}
+
