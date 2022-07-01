2223a4513742eafe7d97a42462523473259f4b57
Olivia Kim <ogkim@berkeley.edu>
2022-07-01 13:27:52 -0700

    firsgcommit

--- lab07/ListSet.java
+++ lab07/ListSet.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class ListSet implements SimpleSet {
 
     List<Integer> elems;
+    private int size;
 
     public ListSet() {
         elems = new ArrayList<Integer>();
@@ -14,21 +15,29 @@ public class ListSet implements SimpleSet {
 
     /** Adds k to the set. */
     public void add(int k) {
-        // TODO
+        size ++;
+        elems.add(k);
     }
 
     /** Removes k from the set. */
     public void remove(int k) {
+        size -= 1;
         Integer toRemove = k;
         // TODO - use the above variable with an appropriate List method.
         // The reason is beyond the scope of this lab, but involves
         // method resolution.
+        elems.remove(toRemove);
+
     }
 
     /** Return true if k is in this set, false otherwise. */
     public boolean contains(int k) {
         // TODO
+        if (elems.contains(k)) {
+            return true;
+        }
         return false;
+
     }
 
     /** Return true if this set is empty, false otherwise. */
@@ -39,12 +48,17 @@ public class ListSet implements SimpleSet {
     /** Returns the number of items in the set. */
     public int size() {
         // TODO
-        return 0;
+        return size;
     }
 
     /** Returns an array containing all of the elements in this collection. */
     public int[] toIntArray() {
         // TODO - use a for loop!
-        return null;
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = elems.get(i);
+        }
+        return arr;
+
     }
 }
