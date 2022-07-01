aa6badf29132d17ee6f007107c9aea3b6fc0028e
Olivia Kim <ogkim@berkeley.edu>
2022-07-01 14:04:09 -0700

    commit 2

--- lab07/BooleanSetTest.java
+++ lab07/BooleanSetTest.java
@@ -5,24 +5,29 @@ public class BooleanSetTest {
 
     @Test
     public void testBasics() {
-        BooleanSet aSet = new BooleanSet(100);
-        assertEquals(0, aSet.size());
-//        aSet.add(3);
-//        assertTrue(aSet.contains(3));
-//        aSet.remove(3);
-//        assertFalse(aSet.contains(3));
+//        BooleanSet aSet = new BooleanSet(100);
+//        assertEquals(0, aSet.size());
 
-        for (int i = 0; i < 100; i += 2) {
-            aSet.add(i);
-            assertTrue(aSet.contains(i));
-        }
-        assertEquals(50, aSet.size());
+        BooleanSet bSet = new BooleanSet(10);
+        bSet.add(3);
+        bSet.add(4);
+        bSet.add(5);
+        bSet.add(10);
+        bSet.remove(10);
+        bSet.toIntArray();
 
-        for (int i = 0; i < 100; i += 2) {
-            aSet.remove(i);
-            assertFalse(aSet.contains(i));
-        }
-        assertTrue(aSet.isEmpty());
-        assertEquals(0, aSet.size());
+//
+//        for (int i = 0; i < 100; i += 2) {
+//            aSet.add(i);
+//            assertTrue(aSet.contains(i));
+//        }
+//        assertEquals(50, aSet.size());
+//
+//        for (int i = 0; i < 100; i += 2) {
+//            aSet.remove(i);
+//            assertFalse(aSet.contains(i));
+//        }
+//        assertTrue(aSet.isEmpty());
+//        assertEquals(0, aSet.size());
     }
 }
