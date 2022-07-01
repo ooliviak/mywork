c48523fbf376f72fcf5cb8e10a656ee8ae750fc3
Olivia Kim <ogkim@berkeley.edu>
2022-06-30 22:23:43 -0700

    commit 3

--- lab06/SLListTest.java
+++ lab06/SLListTest.java
@@ -23,4 +23,26 @@ public class SLListTest {
         test1.add(0, 0);
         assertEquals(SLList.of(0, 1, 2, 3, 4, 5), test1);
     }
+
+    @Test
+    public void testReverse() {
+
+//        general case  (size >= 2)
+        SLList test1 = SLList.of(1, 3, 5, 6);
+        assertEquals(6, test1.get(0));
+        assertEquals(3, test1.get(2));
+
+
+//        base case 1   (size = 1)
+        SLList test3 = new SLList(2);
+        assertEquals(2, test3.get(0));
+
+
+//        base case 2   (size = 0)
+        SLList test4 = new SLList();
+        assertEquals(null, test4.get(0));
+
+
+
+    }
 }
