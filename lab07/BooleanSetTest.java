7874e4aa7cbe540342dad03e34c2ada6cb3be69f
Olivia Kim <ogkim@berkeley.edu>
2022-07-01 14:32:56 -0700

    commit 3

--- lab07/BooleanSetTest.java
+++ lab07/BooleanSetTest.java
@@ -5,8 +5,8 @@ public class BooleanSetTest {
 
     @Test
     public void testBasics() {
-//        BooleanSet aSet = new BooleanSet(100);
-//        assertEquals(0, aSet.size());
+        BooleanSet aSet = new BooleanSet(100);
+        assertEquals(0, aSet.size());
 
         BooleanSet bSet = new BooleanSet(10);
         bSet.add(3);
@@ -16,18 +16,18 @@ public class BooleanSetTest {
         bSet.remove(10);
         bSet.toIntArray();
 
-//
-//        for (int i = 0; i < 100; i += 2) {
-//            aSet.add(i);
-//            assertTrue(aSet.contains(i));
-//        }
-//        assertEquals(50, aSet.size());
-//
-//        for (int i = 0; i < 100; i += 2) {
-//            aSet.remove(i);
-//            assertFalse(aSet.contains(i));
-//        }
-//        assertTrue(aSet.isEmpty());
-//        assertEquals(0, aSet.size());
+
+        for (int i = 0; i < 100; i += 2) {
+            aSet.add(i);
+            assertTrue(aSet.contains(i));
+        }
+        assertEquals(50, aSet.size());
+
+        for (int i = 0; i < 100; i += 2) {
+            aSet.remove(i);
+            assertFalse(aSet.contains(i));
+        }
+        assertTrue(aSet.isEmpty());
+        assertEquals(0, aSet.size());
     }
 }
