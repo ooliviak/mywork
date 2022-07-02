5494e251d67410a563c5147e86fe00d96d5074db
Olivia Kim <ogkim@berkeley.edu>
2022-07-02 15:27:26 -0700

    commit 3

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -4,7 +4,8 @@ public class ArrayDeque<T> implements Deque<T> {
 
     public T[] items;
     private int size;
-    private int capacity;
+
+//    private int capacity;
     private int nextLast;
     private int nextFirst;
 
@@ -12,31 +13,40 @@ public class ArrayDeque<T> implements Deque<T> {
     public ArrayDeque() {
         items = (T[]) new Object[8];
         size = 0;
-
-
+        nextFirst = 1;
+        nextLast = 0;
     }
+    /* source - proj1 tip slide */
     private void resize(int capacity) {
+        /* capacity = size + 1 */
         T[] newarr = (T[]) new Object[capacity];
         System.arraycopy(items, 0, newarr, 0, size);
         items = newarr;
     }
 
-    public void addLast(int x) {
+    @Override
+    public void addFirst(T item) {
         if (size == items.length) {
             resize(size + 1);
         }
-        items[nextLast] = x;
+        items[nextFirst] = item;
         size += 1;
+        if (nextFirst == 0) {
+            nextFirst = items.length - 1;
+        } else {
+            nextFirst = nextFirst - 1;
+        }
     }
 
-    @Override
-    public void addFirst(T item) {
-
-    }
-
+    /* source - proj1 tip slide */
     @Override
     public void addLast(T item) {
-
+        if (size == items.length) {
+            resize(size + 1);
+        }
+        items[nextLast] = item;
+        size += 1;
+        nextLast = nextLast + 1;
     }
 
     @Override
