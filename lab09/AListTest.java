6a4ced88b5434cee168f6f517a6e8d0ac9bdfb8a
Olivia Kim <ogkim@berkeley.edu>
2022-07-07 15:32:54 -0700

    commit 2

--- lab09/AListTest.java
+++ lab09/AListTest.java
@@ -18,13 +18,18 @@ public class AListTest {
         }
         assertEquals(6, count);
 
-//
-//        AList friends = AList.of(5, 23, 42);
-//        Iterator<Integer> seer = friends.iterator();
-//        while (seer.hasNext()) {
-//            int x = seer.next();
-//            System.out.println(x);
 
+        AList friends = AList.of(5, 23, 42);
+        Iterator<Integer> seer = friends.iterator();
+        while (seer.hasNext()) {
+            int x = seer.next();
+            System.out.println(x);
+        }
+        Iterator<Integer> seer1 = friends.iterator();
+        while (seer1.hasNext()) {
+            int x = seer1.next();
+            System.out.println(x);
+        }
     }
 
 
