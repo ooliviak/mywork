0c95a558ecd4d51d377d33f17d113d4266761795
Olivia Kim <ogkim@berkeley.edu>
2022-07-13 11:28:45 -0700

    commit

--- proj2/gitlet/Branch.java
+++ proj2/gitlet/Branch.java
@@ -1,2 +1,17 @@
-package gitlet;public class Branch {
+package gitlet;
+
+public class Branch {
+
+    private String mainBranch;
+    private String headBranch;
+    private String mainBranchId;
+
+    public Branch(Commit commitZero) {
+        this.mainBranch = "main";
+        this.headBranch = "HEAD";
+        Utils.join(Repository.BRANCH_DIR, mainBranch);
+        Utils.join(Repository.BRANCH_DIR, headBranch);
+
+    }
+
 }
